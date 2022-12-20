5a513e227736ea124ea690ad73cf19040443282f
Brandonjca <brandito144@gmail.com>
2022-12-19 22:03:57 -0500

    proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NoFoundComponent } from './no-found/no-found.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { NavLeftComponent } from './shared/nav-left/nav-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NoFoundComponent,
+    CategoryComponent,
+    DashboardComponent,
+    ProductComponent,
+    BreadcrumbsComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    NavLeftComponent
   ],
   imports: [
     BrowserModule
